9a7daf685179de1c5d1b6fc5cf38375a93a8b481
Teresia <teresiamwangi26@gmail.com>
2023-03-12 15:21:33 +0300

    Complete Assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(){
+}
+function subtract(){
+}
+function multiply(){
+}
+function divide(){
+}
+
+let a = 23;
+let b = 3;
+function add(a, b){
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}
+let n = 30
+function increment(n){
+    ++n;
+    return n;
+}
+function decrement(n){
+    return --n;
+}
+function  makeInt(n){
+    return parseInt(n, 10);
+}
+let x = 2.222
+function preserveDecimal(x){
+    return parseFloat(x);
+}
